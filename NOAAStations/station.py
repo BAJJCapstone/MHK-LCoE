6abbb9236d4a4d5381fd9702b7c76280cdff1159
JASON CAIN <cain.ja@husky.neu.edu>
2017-02-16 13:34:39 -0500

    added conversion tools

--- NOAAStations/station.py
+++ NOAAStations/station.py
@@ -3,3 +3,7 @@ class Station:
     def __init__(self, ID):
         self.ID = ID
         self.getStationInfo()
+
+    def convertLatLon(self, tude):
+        multiplier = 1 if tude[-1] in ['N', 'E'] else -1
+        return multiplier * sum(float(x) / 60 ** n for n, x in enumerate(tude[:-3].split('° ')))
--- NOAAStations/tidalStation.py
+++ NOAAStations/tidalStation.py
@@ -17,9 +17,32 @@ import os.path
 class TidalStation(Station):
 
     def getStationInfo(self):
+        station_url = 'https://tidesandcurrents.noaa.gov/stationhome.html?id={}'.format(station_id)
+        with urllib.request.urlopen(station_url) as url:
+            station_html = url.read()
 
+        soup = BeautifulSoup(station_html, "html.parser")
+        table = soup.find_all('tr') #find the table of available data
+        got_latitude = False
+        got_longitude = False
+        try:
+            for table_row in table: # skip the header
+                data_type = table_row.find_all('td')[0].get_text()
+                if 'Latitude' in data_type and not got_latitude:
+                    latitude = table_row.find_all('td')[1].get_text()
+                    got_latitude = True
+                if 'Longitude' in data_type and not got_longitude:
+                    longitude = table_row.find_all('td')[1].get_text()
+                    got_longitude = True
+                if got_longitude and got_latitude:
+                    break
+                else:
+                    raise
+        except:
+            print('STATION DATA RETRIEVAL FAILURE')
+            return
 
-        self.latitude =
-        self.longitude =
+        self.latitude = self.convertLatLon(latitude)
+        self.longitude = self.convertLatLon(longitude)
 
     def getAvailableData(self):
